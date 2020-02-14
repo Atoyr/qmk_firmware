6d5e36cec728f38c5b26da90d9b1913d0bf60f34
Atoyr <lessironglance@gmail.com>
2020-02-14 19:11:53 +0900

    update layer RGB light color

--- keyboards/crkbd/keymaps/atoyr/keymap.c
+++ keyboards/crkbd/keymaps/atoyr/keymap.c
@@ -166,9 +166,11 @@ uint32_t layer_state_set_user(layer_state_t state) {
     case _RAISE:
       rgblight_sethsv_noeeprom_red();
       break;
+    case _ADJUST:
+      rgblight_sethsv_noeeprom_green();
+      break;
     default: // for any other layers, or the default layer
-      rgblight_mode(RGBLIGHT_MODE_STATIC_GRADIENT + 3);
-      rgblight_sethsv_red();
+      rgblight_mode(RGB_current_mode);
       break;
   }
   return state;
