a02babf91d1c7b602b8a14e499397cb11b41a8d5
atoyr <lessironglance@gmail.com>
2020-09-29 19:24:07 +0900

    update ZenHan

--- keyboards/dz60/keymaps/mykeymap/keymap.c
+++ keyboards/dz60/keymaps/mykeymap/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_NUBS,  KC_GRV,     \
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,              \
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,                       \
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT(_FN,RALT(KC_GRV)),  \
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT(_FN,MY_USZH),  \
       MO(_FN), KC_LGUI, KC_LALT,           KC_SPC,           KC_SPC,           KC_SPC,          KC_RALT, _______, KC_LEFT, KC_DOWN, KC_RGHT      \
       ),
 
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_NUBS,  KC_GRV,     \
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,              \
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,                       \
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT(_FN,RALT(KC_GRV)),  \
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT(_FN,MY_USZH),  \
       MO(_FN), KC_LGUI, KC_LALT,           KC_SPC,           KC_SPC,           KC_SPC,          KC_RALT, _______, KC_LEFT, KC_DOWN, KC_RGHT      \
       ),
 
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //| Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 | Ins | Del |
        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,     \
       _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,              \
-      _______, BL_TOGG, BL_STEP,  BL_INC,  BL_DEC, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,                       \
+      _______, BL_TOGG, BL_STEP,  BL_INC,  BL_DEC, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,                       \
       _______         , KC_BTN1, KC_BTN3, KC_BTN2, _______,TG(_ADJ), _______, _______, _______, _______, _______, _______, KC_MS_U, _______,     \
       _______, _______, _______,           KC_ENT,           KC_ENT,           KC_ENT,          _______, _______, KC_MS_L, KC_MS_D, KC_MS_R      \
       ),
@@ -112,8 +112,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MY_USZH:
       if (record->event.pressed) {
         // Do something when pressed
+        register_code(KC_RALT);
+        register_code(KC_GRV);
       } else {
         // Do something else when release
+        unregister_code(KC_GRV);
+        unregister_code(KC_RALT);
       }
       return false; // Skip all further processing of this key
     default:
