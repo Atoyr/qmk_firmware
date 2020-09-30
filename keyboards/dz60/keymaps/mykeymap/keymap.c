60a4703ba157d6dfd3baf613e69bb90a35c1775e
atoyr <lessironglance@gmail.com>
2020-09-30 13:32:34 +0900

    update keymaps

--- keyboards/dz60/keymaps/mykeymap/keymap.c
+++ keyboards/dz60/keymaps/mykeymap/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_NUBS,  KC_GRV,     \
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC, KC_BSPC,              \
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,  KC_ENT,                       \
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT(_FN,MY_USZH),  \
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,  RSFT_T(KC_SLSH),   KC_UP, LT_FN_USZH,
       MO(_FN), KC_LGUI, KC_LALT,           KC_SPC,           KC_SPC,           KC_SPC,          KC_RALT, _______, KC_LEFT, KC_DOWN, KC_RGHT      \
       ),
 
@@ -135,11 +135,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_FN);
         if (is_tapped) {
-          tap_code(MY_USZH);
+          register_code(KC_RALT);
+          register_code(KC_GRV);
+          unregister_code(KC_GRV);
+          unregister_code(KC_RALT);
         }
-        // Do something else when release
-        // unregister_code(KC_GRV);
-        // unregister_code(KC_RALT);
       }
       return false; // Skip all further processing of this key
     default:
