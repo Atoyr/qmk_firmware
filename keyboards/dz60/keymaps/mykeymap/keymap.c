0fd5ff958feb8ea06da63ad463268950953b2354
atoyr <lessironglnace@gmail.com>
2020-12-11 02:06:35 +0900

    update keymaps

--- keyboards/dz60/keymaps/mykeymap/keymap.c
+++ keyboards/dz60/keymaps/mykeymap/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_JP] = LAYOUT_directional(  \
-       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,S(KC_MINS), KC_INT3, S(KC_LBRC), \
-       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RBRC, KC_NUHS, KC_BSPC,              \
+       KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,S(KC_MINS), KC_BSPC , S(KC_LBRC), \
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_RBRC, KC_NUHS, KC_INT3,              \
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, S(KC_7),  KC_ENT,                       \
-      MO(_JP_SHIFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______,LT(_JP_SHIFT,KC_SLSH),   KC_UP, LT(_FN,KC_GRV),
-      MO(_FN), KC_LGUI, KC_LALT,           KC_SPC,           KC_SPC,           KC_SPC,          KC_RALT, _______, KC_LEFT, KC_DOWN, KC_RGHT      \
+      MO(_JP_SHIFT),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, _______, KC_SLSH,   MO(_JP_SHIFT), LT(_FN,KC_GRV),
+      MO(_FN), KC_LGUI, KC_LALT,           KC_SPC,           KC_SPC,           KC_SPC,          KC_RALT, _______, KC_LEFT, S(KC_LBRC), S(KC_LBRC)      \
       ), 
 
   /* JP_SHIFT
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 
   [_JP_SHIFT] = LAYOUT_directional(  \
-       S(KC_ESC), S(KC_1), KC_LBRC, S(KC_3), S(KC_4), S(KC_5),  KC_EQL, S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9),S(KC_INT1),S(KC_SCLN), S(KC_INT3), S(KC_EQL),     \
-       S(KC_TAB), S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), S(KC_RBRC),S(KC_NUHS), S(KC_BSPC),              \
+       S(KC_ESC), S(KC_1), KC_LBRC, S(KC_3), S(KC_4), S(KC_5),  KC_EQL, S(KC_6), S(KC_QUOT), S(KC_8), S(KC_9),S(KC_INT1),S(KC_SCLN), KC_BSPC, ,     \
+       S(KC_TAB), S(KC_Q), S(KC_W), S(KC_E), S(KC_R), S(KC_T), S(KC_Y), S(KC_U), S(KC_I), S(KC_O), S(KC_P), S(KC_RBRC),S(KC_NUHS), S(KC_INT3),              \
       S(KC_LCTL), S(KC_A), S(KC_S), S(KC_D), S(KC_F), S(KC_G), S(KC_H), S(KC_J), S(KC_K), S(KC_L), KC_QUOT, S(KC_2), S(KC_ENT),                       \
-      _______, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M),S(KC_COMM),S(KC_DOT), _______,S(KC_SLSH),S(KC_UP),KC_INT5,         \
-      MO(_FN), S(KC_LGUI), S(KC_LALT),        S(KC_SPC),        S(KC_SPC),        S(KC_SPC),       S(KC_RALT), _______,S(KC_LEFT),S(KC_DOWN),S(KC_RGHT)      \
+      _______, S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_N), S(KC_M),S(KC_COMM),S(KC_DOT), _______,S(KC_SLSH),_______ ,KC_INT5,         \
+      MO(_FN), S(KC_LGUI), S(KC_LALT),        S(KC_SPC),        S(KC_SPC),        S(KC_SPC),       S(KC_RALT), _______,S(KC_LEFT),S(KC_EQL) ,S(KC_EQL)      \
       ), 
 
 
@@ -101,9 +101,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_FN] = LAYOUT_directional(
        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,     \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______,              \
-      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______, _______,                       \
-      _______         , KC_BTN1, KC_BTN3, KC_BTN2, _______,TO(_ADJ), _______, _______, _______, _______, _______, _______, KC_MS_U, _______,     \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, KC_PSCR, _______,              \
+      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_LEFT, KC_RGHT, _______,                       \
+      KC_LSFT         , KC_BTN1, KC_BTN3, KC_BTN2, _______,TO(_ADJ), _______, _______, _______, _______, _______, KC_DOWN, KC_MS_U, _______,     \
       _______, _______, _______,           KC_ENT,           KC_ENT,           KC_ENT,          _______, _______, KC_MS_L, KC_MS_D, KC_MS_R      \
       ),
 
